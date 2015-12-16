737be00ad80fcc6dfaf8317920d0907665a266e3
krthk2005 <krthk.2005@gmail.com>
2015-12-16 05:12:02 -0600

    Update homeController.js

--- partials/home-folder/homeController.js
+++ partials/home-folder/homeController.js
@@ -3,3 +3,27 @@ var app = angular.module('myApp.home', []);
 app.controller('HomeCtrl', ['$scope', function($scope){
 	$scope.test = "success detail page";
 }])
+app.directive('spy', spy);
+
+
+  function spy() {
+    return {
+      restrict: 'A',
+      link: link
+    };
+
+    function link(scope, element, attrs) {
+
+      // Dom ready
+      $(function () {
+        if (attrs.spy === 'affix') {
+          $(element).affix({
+            offset: {
+              top: attrs.offsetTop || 10
+              // bottom: attrs.offsetBottom || 10
+            }
+          });
+        }
+      });
+    }
+  }
