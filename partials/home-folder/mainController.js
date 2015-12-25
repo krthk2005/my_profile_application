9c68f41f73208d54020d16cabd7d1a3cd1a29022
krthk2005 <krthk.2005@gmail.com>
2015-12-25 22:32:33 +0530

    perfect
    perfect

--- partials/home-folder/mainController.js
+++ partials/home-folder/mainController.js
@@ -5,4 +5,23 @@ app.controller('MainCtrl', ['$scope', function($scope){
 	$scope.displayContent = function(tab){
 		$scope.tab = tab;
 	};
+	$(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
+   
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+  });
+
 }])
